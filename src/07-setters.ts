6e0d4acc1acab147776b49ba402a44ff6279043e
IvanaMech <ivanamellone@gmail.com>
2022-11-22 15:14:19 -0300

    getter and setter

--- src/07-setters.ts
+++ src/07-setters.ts
@@ -2,13 +2,13 @@ export class MyDate{
   // para utilizar el constructor se debe si o si declarar si es publico o privado (el acceso) para que se automatize todo
   constructor(
     public year: number = 1993,
-    public month:number = 7,
+    public _month:number = 7,
     private _day: number = 9
     ){}
 
   printFormat(): string {
     const day = this.addPadding(this._day);
-    const month = this.addPadding(this.month);
+    const month = this.addPadding(this._month);
     return `${day}/${month}/${this.year}`;
   }
 
@@ -24,7 +24,7 @@ export class MyDate{
       this._day += amount;
     }
     if(type === 'months'){
-      this.month += amount;
+      this._month += amount;
     }
     if(type === 'years'){
       this.year += amount;
@@ -40,20 +40,23 @@ export class MyDate{
     if(this.year % 100 === 0) return false;
     return this.year % 4 === 0;
   }
+
+  get month(){
+    return this._month;
+  }
+  set month(newValue: number){
+    if( newValue >=1 && newValue <= 12){
+    this._month = newValue;
+    }else {
+      throw new Error('month out of range')
+    }
+
+  }
 }
 
 const myDate = new MyDate(1993,7,10);
 console.log(myDate.printFormat());
-//console.log(myDate.getDay());
-console.log(myDate.day);
-console.log('1993',myDate.isLeapYear);
-
-const myDate2 = new MyDate(2000, 7, 10);
-console.log('2000', myDate2.isLeapYear);
-
-const myDate3 = new MyDate(2001, 7, 10);
-console.log('2001', myDate3.isLeapYear);
-
-const myDate4 = new MyDate(2004, 7, 10);
-console.log('2004', myDate4.isLeapYear);
-
+myDate._month = 4;
+console.log('run', myDate.month);
+myDate.month = 10;
+console.log('esto no debe aparecer', myDate.month);
